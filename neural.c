9b9b2c16fac70d04567cf2224a9a9aae91470b98
Quentin SANTOS <quentin.santos@worldonline.fr>
2015-02-14 21:39:19 +0100

    Inputs are neurons

--- neural.c
+++ neural.c
@@ -13,7 +13,6 @@ float sigmoid_prime(float x)
     return (1 - s) * s;
 }
 
-float input[3];
 float weights[3];
 
 typedef struct neuron neuron_t;
@@ -25,12 +24,14 @@ struct neuron
     float output;
 };
 
+neuron_t neurons[4];
+
 float neuron_propagate(neuron_t* neuron)
 {
     // compute local field δ_i = sum(y_j w_ij)
     float local_field = 0;
     for (size_t i = 0; i < 3; i++)
-        local_field += input[i] * weights[i];
+        local_field += neurons[i].output * weights[i];
     neuron->local_field = local_field;
 
     // compute output y_i
@@ -45,36 +46,38 @@ void neuron_backpropagate(neuron_t* neuron)
     float local_gradient = neuron->local_gradient * sigmoid_prime(neuron->local_field);
 
     for (size_t i = 0; i < 3; i++)
-        weights[i] -= local_gradient * input[i];
+        weights[i] -= local_gradient * neurons[i].output;
 }
 
 int main()
 {
-    input[0] = 1.f;
-    neuron_t neuron;
+    neurons[0].output = 1.f;
+
+    neuron_t* neuron = &neurons[3];
+
     for (int i = 0; i < 100000; i++)
     {
         int a = rand() & 1;
         int b = rand() & 1;
         int output = a | b;
 
-        input[1] = (float)a;
-        input[2] = (float)b;
+        neurons[1].output = (float)a;
+        neurons[2].output = (float)b;
 
-        float result = neuron_propagate(&neuron);
+        float result = neuron_propagate(neuron);
 
         float correction = result - output;
-        neuron.local_gradient = correction;
-        neuron_backpropagate(&neuron);
+        neuron->local_gradient = correction;
+        neuron_backpropagate(neuron);
     }
 
     for (int a = 0; a <= 1; a++)
         for (int b = 0; b <= 1; b++)
         {
             int output = a | b;
-            input[1] = (float)a;
-            input[2] = (float)b;
-            float result = neuron_propagate(&neuron);
+            neurons[1].output = (float)a;
+            neurons[2].output = (float)b;
+            float result = neuron_propagate(neuron);
             printf("%i %f\n", output, result);
         }
 
